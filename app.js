c1f8213998d16709f6218fbd92e791ec37f8dbba
Dcruz05 <dcruz11@ucol.mx>
2023-02-28 08:08:54 -0600

    Primer commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+});
+app.get('/one', (req, res) => {
+    res.send("Hello World from route ONE!");
+});
+app.get('/prueba', (req, res) => {
+    res.send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Test</h1>
+    </body>
+    </html>`);
+});
+app.listen(3000);
